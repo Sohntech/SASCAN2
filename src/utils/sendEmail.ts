7a1816782658c95c885bb6b7e0cb728248ee1de1
Sohntech <ndiagalo259@gmail.com>
2025-03-01 13:40:04 +0000

    feat:custom sender email

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -13,11 +13,17 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
   });
 
   const mailOptions = {
-    from: 'Orange Digital Center - Sonatel Academy',
+    from: `"Orange Digital Center - Sonatel Academy" <${process.env.SMTP_USER}>`, // Alias avec email réel
     to: email,
     subject: "Votre code de réinitialisation de mot de passe",
     text: `Votre code de réinitialisation : ${token}`,
-    html: `<p><b>Votre code de réinitialisation :</b> ${token}</p>`,
+    html: `
+      <p>Bonjour,</p>
+      <p>Vous avez demandé une réinitialisation de votre mot de passe.</p>
+      <p><b>Code de réinitialisation :</b> <span style="font-size: 18px; color: blue;">${token}</span></p>
+      <p>Si vous n'êtes pas à l'origine de cette demande, ignorez simplement cet email.</p>
+      <p>Cordialement,<br><b>Orange Digital Center - Sonatel Academy</b></p>
+    `,
   };
 
   await transporter.sendMail(mailOptions);
