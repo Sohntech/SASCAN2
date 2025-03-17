5599bbf7c205e15367ac08af0cb9fdbbccbdfa0d
Ndiaga Lo <macbookpro@192.168.1.252>
2025-03-17 15:03:16 +0000

    feat: update node cron to 15H10

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.use('/api/presences', presenceRoutes);
 app.use('/uploads', express.static('uploads'));
 
 // Tâche cron pour marquer les absents à 16h
-cron.schedule('00 13 * * 1-5', async () => { // 
+cron.schedule('10 15 * * 1-5', async () => { // 
   console.log('--- --- --- Début de la tâche cron : marquage des absences (lundi à vendredi à 16h00) !!!');
   await markAbsentAtFourPM();
   console.log('--- --- --- Fin de la tâche cron !!!');
